6c93c67f8bd07cd48e2d6969a22a733514c05528
Muhammad Seeban Usman <muhammadseebanusman@gmail.com>
2026-07-11 20:24:28 +0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+a = 3 #int
+b = 7.52 #float
+c = "hello 12 !@" #string
+d = True #boolean
+e = False #boolean
+f = "" # string but will not take space in ram
+g = " " #string but its not same as nun and white blank is a character and takes space in memory
+f = None #None - Type
